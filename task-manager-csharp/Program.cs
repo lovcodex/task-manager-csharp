3264023c1b8ab694fe9831fa4ad449d70aa1e66a
Lorena <lorenasilvaapalm@gmail.com>
2026-04-16 11:53:47 -0300

    Commit -  criação da classe tarefas, métodos rodando corretamente

--- task-manager-csharp/Program.cs
+++ task-manager-csharp/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using task_manager_csharp;
+
+var tarefa = new Tarefa
+{
+    Titulo = "Estudar POO",
+    Descricao = "Aprender classes e objetos",
+    Status = Tarefa.StatusTarefa.Pendente,
+    Prioridade = 1
+};
+
+Console.WriteLine(tarefa);
+
+tarefa.AlterarStatus(Tarefa.StatusTarefa.EmProgresso);
+
+Console.WriteLine(tarefa);
---
+++ task-manager-csharp/Tarefa.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_manager_csharp
+{
+    public class Tarefa
+    {
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+
+        public enum StatusTarefa
+        {
+            Pendente,
+            EmProgresso,
+            Concluida
+        }
+
+        public StatusTarefa Status { get; set; }
+
+        public int Prioridade { get; set; }
+
+        public void AlterarStatus(StatusTarefa novoStatus)
+        {
+            Status = novoStatus;
+        }
+
+        public override string ToString()
+        {
+            return $"{Titulo} - {Status} - Prioridade: {Prioridade}";
+        }
+    }
+}
